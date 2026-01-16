5c8cd78f37d16d759dd3bc4a1f5811e965a4b15b
SublimedApple58 <tizianofelice@gmail.com>
2026-01-16 18:57:49 +0100

    build success

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero';
 import Integrations from '../components/Integrations';
 import Benefits from '../components/Benefits';
-import Testimonials from '../components/Testimonials';
 import FinalCTA from '../components/FinalCTA';
 
 export default function HomePage() {
